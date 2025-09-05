1e3bdcdca4e66f13278666203c0ea01ff770c622
JRowe47 <jrowe47@gmail.com>
2025-09-05 15:04:49 -0600

    Add Tiny Shakespeare data loader and 10M demo

--- ironcortex/__init__.py
+++ ironcortex/__init__.py
@@ -1,5 +1,14 @@
-from .config import CortexConfig
-from .model import CortexReasoner
-from .training import LossWeights, train_step
-from .generation import generate
-from .wiring import hex_neighbors_grid, hex_axial_coords_from_grid
+from .config import CortexConfig as CortexConfig
+from .model import CortexReasoner as CortexReasoner
+from .training import LossWeights as LossWeights, train_step as train_step
+from .generation import generate as generate
+from .wiring import (
+    hex_neighbors_grid as hex_neighbors_grid,
+    hex_axial_coords_from_grid as hex_axial_coords_from_grid,
+)
+from .data import (
+    download_tiny_shakespeare as download_tiny_shakespeare,
+    load_tiny_shakespeare as load_tiny_shakespeare,
+    TextDiffusionDataset as TextDiffusionDataset,
+    TextDiffusionSample as TextDiffusionSample,
+)
